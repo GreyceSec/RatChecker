3e0383f09fc4c6869966c43590aba6fef1d0bf5c
GreyceSec <sharov1589@icloud.com>
2026-05-12 15:10:57 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,9 +25,9 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include "imgui.h"
-#include "imgui_impl_win32.h"
-#include "imgui_impl_dx11.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_win32.h"
+#include "imgui/imgui_impl_dx11.h"
 #include <d3d11.h>
 
 
